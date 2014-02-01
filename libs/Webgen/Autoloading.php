76406f5f0171db3fd94ddd185447e06cfd138907
Jan Pecha <janpecha@email.cz>
2014-02-01 22:18:08 +0100

    Added support for loading of libraries
    Support:
    Composer /vendor/ dir (/vendor/autoload.php)
    Own loader file: (/libs/autoload.php)
    RobotLoader: scanning all files in /libs/
    
    Added new config options:
    input.libsDir (libs)
    input.composerFile (composer.json)
    output.cacheDir (cache)
    
    Info about RobotLoader: http://doc.nette.org/cs/2.0/auto-loading

---
+++ libs/Webgen/Autoloading.php
@@ -0,0 +1,83 @@
+<?php
+	/**
+	 * Webgen - static website generator written in PHP
+	 *
+	 * @author  Jan Pecha, <janpecha@email.cz>
+	 */
+
+	namespace Webgen;
+
+	use Nette\Utils;
+
+	class Autoloading extends \Nette\Object
+	{
+		private $composerFile;
+
+		private $libsDir;
+
+
+		public function __construct($composerFile, $libsDir)
+		{
+			$this->composerFile = $composerFile;
+			$this->libsDir = $libsDir;
+		}
+
+
+
+		public function autoload()
+		{
+			$files = array();
+
+			// Composer
+			if ($this->composerFile !== NULL) {
+				$json = @file_get_contents($this->composerFile);
+
+				if (is_string($json)) {
+					$data = Utils\Json::decode($json, Utils\Json::FORCE_ARRAY);
+					$composerDir = 'vendor';
+
+					if (isset($data['config']['vendor-dir'])) {
+						$composerDir = $data['config']['vendor-dir'];
+					}
+
+					$files[] = dirname($this->composerFile) . "/$composerDir/autoload.php";
+				}
+			}
+
+			// Own loader
+			$files[] = $this->libsDir . '/autoload.php';
+
+			// Include files
+			if (count($files)) {
+				\Cli\Cli::log('Autoload files...');
+
+				foreach ($files as $file) {
+					if (file_exists($file)) {
+						\Cli\Cli::log($file);
+						Utils\LimitedScope::load($file);
+					} else {
+						\Cli\Cli::log("[not found] $file");
+					}
+				}
+			}
+
+			return $files;
+		}
+
+
+		/**
+		 * @return \Nette\Loaders\RobotLoader|FALSE
+		 */
+		public function createRobotLoader()
+		{
+			if (!file_exists($this->libsDir)) {
+				return FALSE;
+			}
+
+			$loader = new \Nette\Loaders\RobotLoader;
+			$loader->autoRebuild = TRUE;
+			$loader->addDirectory($this->libsDir);
+			return $loader;
+		}
+	}
+
