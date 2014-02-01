72e851fb1e1baeca5d02bc57bd6fa1b14b2157dc
Jan Pecha <janpecha@email.cz>
2014-02-01 22:40:24 +0100

    Examples: added 'external-libs'

---
+++ examples/external-libs/libs/MyLibrary.php
@@ -0,0 +1,20 @@
+<?php
+
+class MyExternalLibrary
+{
+	public static function hello()
+	{
+		return 'Hello world!';
+	}
+
+
+	public static function getItems()
+	{
+		return array(
+			'Item A',
+			'Item B',
+			'Item C',
+		);
+	}
+}
+
